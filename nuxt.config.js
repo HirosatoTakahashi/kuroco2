28eb2c90afec44ac17095b3a10d847a2c03c1cb1
hirosato_takahashi <154598241+HirosatoTakahashi@users.noreply.github.com>
2024-01-30 19:04:23 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://tk16-site.g.kuroco-front.app/',
+    baseURL: '/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
